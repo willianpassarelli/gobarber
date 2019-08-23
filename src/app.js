9fe71527169d8a95384b052e842efac8de6043e1
willianpassarelli <devpassarelli@gmail.com>
2019-08-22 22:03:22 -0300

    Definindo estrutura

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// Para funcionar o import foi instalado a dependência sucrase
+import express from "express";
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
