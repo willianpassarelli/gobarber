258136aa0b4595c98eb02cf61e5e694cb1546bc6
willianpassarelli <devpassarelli@gmail.com>
2019-08-26 21:44:21 -0300

    Upload and associate

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -23,6 +23,11 @@ class User extends Model {
     return this;
   }
 
+  // criando relacionamento do User com File
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 // array para importar os models
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -15,7 +16,9 @@ class Database {
     // inicializa a conexão do postgres com o Sequelize
     this.connection = new Sequelize(databaseConfig);
     // passa a conexão para os models criados
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 export default new Database();
