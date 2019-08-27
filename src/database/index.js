c38e8e499ff84df485c5b18de4975b6b2515eb6a
willianpassarelli <devpassarelli@gmail.com>
2019-08-26 23:17:54 -0300

    Create schedule and listing

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 // array para importar os models
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
