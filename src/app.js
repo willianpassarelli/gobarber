e8e3e9ed02b19cd5939927e7a5a75e4f33eae113
willianpassarelli <devpassarelli@gmail.com>
2019-08-30 00:09:39 -0300

    config .env

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 // Para funcionar o import foi instalado a dependência sucrase
+import 'dotenv/config';
+
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
@@ -38,9 +40,13 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      const errors = await new Youch(err, req).toJSON();
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
 
-      return res.status(500).json(errors);
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
     });
   }
 }
